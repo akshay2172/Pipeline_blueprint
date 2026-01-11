6bbfde986143039c75f4afc50c520aaf0bb303d9
akshay <akshayk22@iitk.ac.in>
2026-01-11 18:50:39 +0530

    Added Backend

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 async function connectDB(uri) {
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
-  console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB - db.js:6");
 }
+
 
 module.exports = { connectDB };
